256f9418159d9ea6db21bef437fddf84bb026bbd
corex <corex@srv867447.hstgr.cloud>
2026-02-01 08:17:45 +0000

    Doctypes

---
+++ memora_admin/memora_admin/doctype/memora_player_profile/memora_player_profile.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, corex and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Memora Player Profile", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ memora_admin/memora_admin/doctype/memora_subject/test_memora_subject.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, corex and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMemoraSubject(FrappeTestCase):
+	pass
